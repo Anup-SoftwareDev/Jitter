265900b3353214253e4b06a6de0bb076affe7ba9
Anup-SoftwareDev <joseph.anupsa@gmail.com>
2022-05-21 12:45:52 +1000

    initial commit

---
+++ src/components/Message.js
@@ -0,0 +1,12 @@
+
+const Message = ({message}) => {
+    return (
+        <>
+            <h4>{message.text}</h4>
+            <p>{message.user}</p>
+        </>
+    )
+
+}
+
+export default Message
